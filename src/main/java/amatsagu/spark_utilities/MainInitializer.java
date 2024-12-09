f4dd249847d20614bd4c5cd72997f3254c0fd871
amatsagu <krzysztof.rapior@protonmail.com>
2024-12-09 18:47:36 +0100

    feat: clean code

--- src/main/java/amatsagu/spark_utilities/MainInitializer.java
+++ src/main/java/amatsagu/spark_utilities/MainInitializer.java
@@ -15,33 +15,19 @@ import org.slf4j.LoggerFactory;
 public class MainInitializer implements ModInitializer {
 	public static final String MOD_ID = "spark_utilities";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	private boolean hasExecutedCommand = false;
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
 		LOGGER.info("Loaded Spark Utilities mod!");
-		ServerLifecycleEvents.SERVER_STARTED.register(this::onServerStart);
+		ServerLifecycleEvents.SERVER_STARTED.register((MinecraftServer server) -> {
+            executeCommand(server, "say Successfully loaded Sparkle Uilities mod.");
+            LOGGER.info("Successfully loaded Spark Utilities mod.");
+        });
 
 		ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
             unlockAllRecipes(handler.getPlayer());
         });
 	}
-
-	private void onServerStart(MinecraftServer server) {
-        if (server.isSingleplayer() && !hasExecutedCommand) {
-            executeCommand(server, "time set 16000");
-            hasExecutedCommand = true;
-        }
-
-        if (server.isDedicated() && !hasExecutedCommand) {
-            executeCommand(server, "time set 16000");
-            hasExecutedCommand = true;
-        }
-    }
 
 	private void unlockAllRecipes(ServerPlayerEntity player) {
         if (player != null && player.getServer() != null) {
